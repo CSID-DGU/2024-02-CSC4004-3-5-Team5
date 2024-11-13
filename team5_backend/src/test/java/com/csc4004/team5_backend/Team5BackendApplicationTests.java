a1d35fd86dd1df0dd7e4240ccf91f2519728a01f
anismynm <ddoe0318@naver.com>
2024-11-14 03:01:05 +0900

    feat: 백엔드 프로젝트 생성 및 초기 설정

---
+++ team5_backend/src/test/java/com/csc4004/team5_backend/Team5BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csc4004.team5_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team5BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
