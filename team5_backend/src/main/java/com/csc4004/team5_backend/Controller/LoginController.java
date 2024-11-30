4a76062dbcbc93f4fe5abe48f484a28cf91d639b
anismynm <ddoe0318@naver.com>
2024-11-30 22:50:43 +0900

    Update LoginController.java

--- team5_backend/src/main/java/com/csc4004/team5_backend/Controller/LoginController.java
+++ team5_backend/src/main/java/com/csc4004/team5_backend/Controller/LoginController.java
@@ -9,9 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.LinkedHashMap;
@@ -24,7 +22,7 @@ public class LoginController {
     private final LoginService loginService;
 
     @GetMapping("/login")
-    public ResponseEntity<?> RegisterLogin(@RequestBody String code, HttpSession session) throws IOException {
+    public ResponseEntity<?> RegisterLogin(@RequestParam("code") String code, HttpSession session) throws IOException {
         Map<String, Object> response = new LinkedHashMap<>();
 
         try {
